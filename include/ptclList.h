7523bc70e39c8157e1df7d869905aaf1d67aa3dc
explos <explosblue@gmail.com>
2025-05-21 12:23:12 +0100

    feat(ui): implement ptcl list widget

---
+++ include/ptclList.h
@@ -0,0 +1,32 @@
+#ifndef PTCLLIST_H
+#define PTCLLIST_H
+
+#include <QWidget>
+#include <QListView>
+#include <QStandardItemModel>
+#include <QVBoxLayout>
+
+#include "ptcl/ptcl.h"
+
+class PtclList : public QWidget
+{
+    Q_OBJECT
+public:
+    void extracted(Ptcl::PtclRes *&res);
+    explicit PtclList(Ptcl::PtclRes *ptclRes, QWidget *parent = nullptr);
+
+private slots:
+    void selectionChanged(const QItemSelection& selection);
+
+signals:
+    void selectedIndexChanged(u32 index);
+
+private:
+    Ptcl::PtclRes* mRes; // TODO: this should not be a raw pointer...
+    QStandardItemModel mListModel;
+    QListView mListView;
+
+    QVBoxLayout mMainLayout;
+};
+
+#endif // PTCLLIST_H
