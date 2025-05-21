3f07b58570b54417e11d96bbb8c3dab6bcc775a9
explos <explosblue@gmail.com>
2025-05-21 12:14:42 +0100

    chore(util): add utility for debug printing

---
+++ include/printUtil.h
@@ -0,0 +1,23 @@
+#ifndef PRINTUTIL_H
+#define PRINTUTIL_H
+
+namespace PrintUtil {
+
+inline const char* createIndentation(size_t indentLevel) {
+
+    size_t totalSpaces = indentLevel * 2;
+
+    char* tabs = new char[totalSpaces + 1];
+
+    for (size_t i = 0; i < totalSpaces; ++i) {
+        tabs[i] = ' ';
+    }
+
+    tabs[totalSpaces] = '\0';
+
+    return tabs;
+}
+
+}
+
+#endif // PRINTUTIL_H
