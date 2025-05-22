daa2386c1fd202ac5eebee1414054a89d2364258
explos <explosblue@gmail.com>
2025-05-22 15:36:20 +0100

    feat(ui): add clickable label

---
+++ src/editor/clickableLabel.cpp
@@ -0,0 +1,18 @@
+#include "editor/clickableLabel.h"
+
+#include <QMouseEvent>
+
+ClickableLabel::ClickableLabel(QWidget* parent) :
+    QLabel(parent) {
+    setCursor(Qt::PointingHandCursor);
+}
+
+void ClickableLabel::mousePressEvent(QMouseEvent* event) {
+    if (event->button() == Qt::LeftButton) {
+        emit clicked();
+    } else if (event->button() == Qt::RightButton) {
+        emit rightClicked();
+    }
+
+    QLabel::mouseMoveEvent(event);
+}
