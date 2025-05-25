bb5b3a111d7c09a1eacca7b6fa944ab8ab30e3ee
explos <explosblue@gmail.com>
2025-05-25 20:09:07 +0100

    feat(ui): layout texture list as a responsive grid

--- include/editor/textureListWidget.h
+++ include/editor/textureListWidget.h
@@ -9,7 +9,7 @@
 #include <QLabel>
 #include <QStyledItemDelegate>
 #include <QPainter>
-
+#include <QScrollArea>
 
 #include <QtMinMax>
 
@@ -19,60 +19,15 @@
 namespace PtclEditor {
 
 
-class TextureListItem final : public QWidget
-{
+class TextureListItem final : public QWidget {
     Q_OBJECT
-
 public:
-    TextureListItem(const QString& text, const QIcon& thumbnail, QWidget* parent = nullptr)
-        : QWidget(parent), mThumbnail(thumbnail) {
-
-        // Create a layout for the custom item
-        QHBoxLayout* layout = new QHBoxLayout(this);
-
-        // Create a label for the thumbnail
-        QLabel* thumbnailLabel = new QLabel(this);
-        thumbnailLabel->setPixmap(mThumbnail.pixmap(64, 64));  // Set size of thumbnail
-        layout->addWidget(thumbnailLabel);
-
-        // Create a label for the text
-        QLabel* textLabel = new QLabel(text, this);
-        layout->addWidget(textLabel);
-
-        // Create a button that only shows on hover (replace/remove)
-        mReplaceButton = new QPushButton(QIcon(":/res/icons/replace.png"), "", this);
-        mRemoveButton = new QPushButton(QIcon(":/res/icons/remove.png"), "", this);
-
-
-        // mReplaceButton->move(geometry().topRight());
-        // I don't like this being hardcoded... (use font relative size maybe?)
-        mRemoveButton->move(geometry().topLeft());
-        mRemoveButton->setFixedSize(16, 16);
-        mRemoveButton->setFlat(true);
-
-        // mRemoveButton->setGeometry(QRect{QPoint{rect().top(), rect().right() + mReplaceButton->width()}, mReplaceButton->size()});
-
-        mReplaceButton->setVisible(false);  // Hidden by default
-        mRemoveButton->setVisible(false);   // Hidden by default
-
-        // layout->addWidget(mReplaceButton);
-        // layout->addWidget(mRemoveButton);
-
-        this->setLayout(layout);
-    }
+    explicit TextureListItem(const QString& text, QIcon thumbnail, QWidget* parent = nullptr);
 
     // TODO: Enter/Leave events => hide/show buttons
 protected:
-    void enterEvent(QEnterEvent* event) final {
-        mRemoveButton->setVisible(true);
-        // mReplaceButton->setVisible(true);
-    }
-
-    void leaveEvent(QEvent* event) final {
-        mRemoveButton->setVisible(false);
-        // mReplaceButton->setVisible(false);
-    }
-
+    void enterEvent(QEnterEvent* event) final;
+    void leaveEvent(QEvent* event) final;
 
 private:
     QIcon mThumbnail;
@@ -81,31 +36,39 @@ private:
 };
 
 
+// ========================================================================== //
+
+
 class TextureListWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit TextureListWidget(QWidget* parent = nullptr);
-
     void setTextures(Ptcl::TextureList* textures);
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
 
 private:
     void populateList();
+    void relayoutGrid();
 
 private:
     // This should probably be a weak ptr?
     Ptcl::TextureList* mTexturesPtr;
 
-    QListWidget mListWidget;
+    QScrollArea mScrollArea;
+    QWidget mGridContainer;
+    QGridLayout mGridLayout;
 
-    QHBoxLayout mMainLayout;
-
-    // TODO: Texture Info (move to dedicated widget?)
-    // QLabel mImageLabel;
-    // QLabel mImageInfoLabel;
-
+    std::vector<TextureListItem*> mItemWidgets;
 };
 
+
+// ========================================================================== //
+
+
 } // namespace PtclEditor
+
 
 #endif // TEXTURELISTWIDGET_H
--- src/editor/textureListWidget.cpp
+++ src/editor/textureListWidget.cpp
@@ -4,71 +4,142 @@
 
 #include "ptcl/ptcl.h"
 
+#include "editor/thumbnailWidget.h"
+
 namespace PtclEditor {
 
+TextureListItem::TextureListItem(const QString& text, QIcon thumbnail, QWidget* parent) :
+    QWidget{parent},
+    mThumbnail{std::move(thumbnail)} {
+
+    // Create a layout for the custom item
+    auto* layout = new QHBoxLayout(this);
+    layout->setAlignment(Qt::AlignTop);
+    layout->setContentsMargins(5, 5, 5, 5);
+
+    // Create a label for the thumbnail
+    ThumbnailWidget* thumbnailWidget = new ThumbnailWidget(this);
+    thumbnailWidget->setPixmap(mThumbnail.pixmap(64, 64));
+    thumbnailWidget->setThumbnailSize(QSize(64, 64));
+    layout->addWidget(thumbnailWidget);
+
+    // Create a label for the text
+    QLabel* textLabel = new QLabel(text, this);
+    textLabel->setWordWrap(true);
+    layout->addWidget(textLabel);
+
+    // Create a button that only shows on hover (replace/remove)
+    mReplaceButton = new QPushButton(QIcon(":/res/icons/replace.png"), "", this);
+    mRemoveButton = new QPushButton(QIcon(":/res/icons/remove.png"), "", this);
+
+    mRemoveButton->move(geometry().topLeft());
+    mRemoveButton->setFixedSize(16, 16);
+    mRemoveButton->setFlat(true);
+
+    mReplaceButton->setVisible(false);  // Hidden by default
+    mRemoveButton->setVisible(false);   // Hidden by default
+
+    setLayout(layout);
+}
+
+void TextureListItem::enterEvent(QEnterEvent* event) {
+    mRemoveButton->setVisible(true);
+    QWidget::enterEvent(event);
+}
+
+void TextureListItem::leaveEvent(QEvent* event) {
+    mRemoveButton->setVisible(false);
+    QWidget::leaveEvent(event);
+}
+
+
+// ========================================================================== //
+
+
 TextureListWidget::TextureListWidget(QWidget *parent) :
-    mTexturesPtr{nullptr}, QWidget{parent} {
+    QWidget{parent},
+    mTexturesPtr{nullptr},
+    mScrollArea{this} {
 
-    setLayout(&mMainLayout);
+    QVBoxLayout* mainLayout = new QVBoxLayout;
 
-    mListWidget.setFlow(QListView::LeftToRight);
-    mListWidget.setWrapping(true);
-    mListWidget.setLayoutMode(QListView::SinglePass);
-    mListWidget.setVerticalScrollMode(QListView::ScrollPerPixel);
-    mListWidget.setMovement(QListView::Static);
+    // Grid Container
+    mGridLayout.setSpacing(10);
+    mGridLayout.setContentsMargins(10, 10, 10, 10);
+    mGridContainer.setLayout(&mGridLayout);
 
-    mListWidget.setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    mListWidget.setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    // Scroll Area
+    mScrollArea.setWidgetResizable(true);
+    mScrollArea.setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    mScrollArea.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    mScrollArea.setWidget(&mGridContainer);
+    mainLayout->addWidget(&mScrollArea);
 
-    mMainLayout.addWidget(&mListWidget);
+    setLayout(mainLayout);
 }
 
 void TextureListWidget::setTextures(Ptcl::TextureList* textures) {
-
     if (mTexturesPtr == textures) {
         return;
     }
 
-    mListWidget.clear();
-
     mTexturesPtr = textures;
-
     populateList();
 }
 
 void TextureListWidget::populateList() {
+    qDeleteAll(mItemWidgets);
+    mItemWidgets.clear();
 
     if (!mTexturesPtr) {
         return;
     }
 
-    mListWidget.clear();
-
     for (auto& texture : *mTexturesPtr) {
-
-        QImage img = texture->textureData();
+        QPixmap pixmap = QPixmap::fromImage(texture->textureData());
         auto format = texture->textureFormat();
 
-        auto width = img.width();
-        auto height = img.height();
+        auto width = pixmap.width();
+        auto height = pixmap.height();
         auto userCount = texture->userCount();
 
-        QPixmap pixmap = QPixmap::fromImage(img);
+        auto text = QString("Format: %1 \nDimentions: %2x%3\n\nUsers: %4")
+            .arg(toString(format))
+            .arg(width)
+            .arg(height)
+            .arg(userCount);
 
-        QListWidgetItem* item = new QListWidgetItem(&mListWidget);
-
-        TextureListItem* textureItem = new TextureListItem(
-            QString("Format: %1 \nDimentions: %2x%3\n\nUsers: %4").arg(toString(format)).arg(width).arg(height).arg(userCount),
-            QIcon(pixmap.scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation))
-            );
-
-        item->setSizeHint(textureItem->sizeHint());
-        mListWidget.setItemWidget(item, textureItem);
-
-        // TODO: this should only occur one time (during initialization?)
-        // Move/handle this in a better way
-        mListWidget.setMinimumHeight(item->sizeHint().height() * 2);
+        auto* textureItem = new TextureListItem(text, QIcon(pixmap), &mGridContainer);
+        textureItem->setFixedSize(200, 120);
+        mItemWidgets.push_back(textureItem);
     }
+    relayoutGrid();
+}
+
+void TextureListWidget::relayoutGrid() {
+    const int cardWidth = 200;
+    const int spacing = mGridLayout.spacing();
+    const int availableWidth = mScrollArea.viewport()->width();
+    const int columns = std::max(1, availableWidth / (cardWidth + spacing));
+
+    QLayoutItem* child;
+    while ((child = mGridLayout.takeAt(0)) != nullptr) {
+        if (QWidget* widget = child->widget()) {
+            mGridLayout.removeWidget(widget);
+        }
+        delete child;
+    }
+
+    for (int i = 0; i < mItemWidgets.size(); ++i) {
+        int row = i / columns;
+        int col = i % columns;
+        mGridLayout.addWidget(mItemWidgets[i], row, col);
+    }
+}
+
+void TextureListWidget::resizeEvent(QResizeEvent* event) {
+    QWidget::resizeEvent(event);
+    relayoutGrid();
 }
 
 } // namespace PtclEditor
