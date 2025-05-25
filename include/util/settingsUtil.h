fe9cf5d18a4b87d4104304d35466a8d47e61b808
explos <explosblue@gmail.com>
2025-05-25 02:33:51 +0100

    feat(util): add settings manager util

---
+++ include/util/settingsUtil.h
@@ -0,0 +1,40 @@
+#ifndef SETTINGSUTIL_H
+#define SETTINGSUTIL_H
+
+#include <QSettings>
+
+
+namespace SettingsUtil {
+
+class SettingsMgr {
+public:
+    static SettingsMgr& instance() {
+        static SettingsMgr sInstance;
+        return sInstance;
+    }
+
+    SettingsMgr(const SettingsMgr&) = delete;
+    SettingsMgr& operator=(const SettingsMgr&) = delete;
+
+    QStringList recentFiles() const;
+    void addRecentFile(const QString& path);
+
+    QString lastOpenPath() const;
+    void setLastOpenPath(const QString& path);
+
+    QString lastSavePath() const;
+    void setLastSavePath(const QString& path);
+
+private:
+    SettingsMgr() = default;
+
+    static constexpr int sMaxRecentFiles = 5;
+
+private:
+    QSettings mSettings;
+};
+
+} // namespace SettingsUtil
+
+
+#endif // SETTINGSUTIL_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #include <QApplication>
 
 int main(int argc, char* argv[]) {
-
     QApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("Explos");
+    QCoreApplication::setApplicationName("PTCLEditor");
+
     PtclEditor::MainWindow win;
     win.show();
 
