c32fd7da37b4a7f503a46804222b1878819535a1
explos <explosblue@gmail.com>
2025-05-28 21:08:13 +0100

    feat(ui): make thumbnail widget clickable

--- include/editor/thumbnailWidget.h
+++ include/editor/thumbnailWidget.h
@@ -11,9 +11,13 @@ public:
     void setPixmap(const QPixmap& pixmap);
     void setThumbnailSize(QSize size);
 
+signals:
+    void clicked();
+
 protected:
     void paintEvent(QPaintEvent* event) override;
     QSize sizeHint() const override;
+    void mousePressEvent(QMouseEvent* event) override;
 
 private:
     QPixmap mPixmap;
