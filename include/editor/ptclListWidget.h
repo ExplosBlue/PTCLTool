62ca98d0ac474e1665e3b543fa005d8556a2ed5a
explos <explosblue@gmail.com>
2025-05-30 14:44:48 +0100

    feat(ui): make EmitterSet list searchable

--- include/editor/ptclListWidget.h
+++ include/editor/ptclListWidget.h
@@ -5,13 +5,13 @@
 #include <QListView>
 #include <QStandardItemModel>
 #include <QVBoxLayout>
+#include <QLineEdit>
 
 #include "ptcl/ptcl.h"
 
 namespace PtclEditor {
 
-class PtclList : public QWidget
-{
+class PtclList : public QWidget {
     Q_OBJECT
 public:
     explicit PtclList(QWidget *parent = nullptr);
@@ -24,15 +24,21 @@ private:
 private slots:
     void selectionChanged(const QItemSelection& selection);
 
+    void filterList(const QString& text);
+
 signals:
     void selectedIndexChanged(u32 index);
 
 private:
-    Ptcl::PtclRes* mResPtr; // TODO: this should not be a raw pointer...
+    Ptcl::PtclRes* mResPtr;
     QStandardItemModel mListModel;
     QListView mListView;
+    QLineEdit mSearchBox;
 
     QVBoxLayout mMainLayout;
+
+    int mSelectedEmitterIndex;
+    std::vector<std::pair<u32, QString>> mEmitterNames;
 };
 
 } //namespace PtclEditor
