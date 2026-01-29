72a2c9517bd330e947c1ce0f7613543bca1b3ead
ExplosBlue <explosblue@gmail.com>
2026-01-29 15:00:19 +0000

    feat(ui): expose flags in basic properties of childEditor

--- include/editor/childEditor/basicPropertiesWidget.h
+++ include/editor/childEditor/basicPropertiesWidget.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "editor/components/enumComboBox.h"
 #include "editor/childEditor/childEditorWidget.h"
 
 #include <QCheckBox>
-#include <QLineEdit>
+#include <QComboBox>
 #include <QWidget>
 
 
@@ -19,15 +18,34 @@ class ChildEditorWidget::BasicPropertiesWidget final : public QWidget {
 public:
     explicit BasicPropertiesWidget(QWidget* parent = nullptr);
 
-    void setProperties(const Ptcl::ChildData::BasicProperties& properties);
+    void setProperties(const Ptcl::ChildData::BasicProperties& properties, bool isFollow, bool isParentField);
 
 signals:
     void propertiesUpdated(const Ptcl::ChildData::BasicProperties& properties);
+    void isFollowUpdated(bool isFollow);
+    void isParentFieldUpdated(bool isField);
+    void isPolygonUpdated(bool isPolygon);
+
+private:
+    void setupConnections();
+    void populateBillboardType();
+
+private:
+    static constexpr std::array<Ptcl::BillboardType, 6> sChildBillboardTypes{
+        Ptcl::BillboardType::Billboard,
+        Ptcl::BillboardType::PolygonXY,
+        Ptcl::BillboardType::PolygonXZ,
+        Ptcl::BillboardType::VelLook,
+        Ptcl::BillboardType::VelLookPolygon,
+        Ptcl::BillboardType::BillboardY,
+    };
 
 private:
     Ptcl::ChildData::BasicProperties mProps{};
 
-    EnumComboBox<Ptcl::BillboardType> mBillboardComboBox{};
+    QComboBox mBillboardComboBox{};
+    QCheckBox mFollowCheckBox{};
+    QCheckBox mParentFieldCheckBox{};
 };
 
 
--- src/editor/childEditor/basicPropertiesWidget.cpp
+++ src/editor/childEditor/basicPropertiesWidget.cpp
@@ -11,26 +11,65 @@ namespace PtclEditor {
 ChildEditorWidget::BasicPropertiesWidget::BasicPropertiesWidget(QWidget* parent) :
     QWidget{parent} {
 
+    for (Ptcl::BillboardType type : sChildBillboardTypes) {
+        mBillboardComboBox.addItem(Ptcl::toString(type), QVariant::fromValue(type));
+    }
+
+    mFollowCheckBox.setText("Follow Parent Emitter");
+    mParentFieldCheckBox.setText("Apply Parent's Field");
+
     auto* mainLayout = new QFormLayout(this);
 
-    // Billboard Type
     mainLayout->addRow("Billboard Type:", &mBillboardComboBox);
-    connect(&mBillboardComboBox, &QComboBox::currentIndexChanged, this, [this]() {
-        const auto type = mBillboardComboBox.currentEnum();
+    mainLayout->addRow("Follow Type:", &mFollowCheckBox);
+    mainLayout->addRow("Field Type:", &mParentFieldCheckBox);
 
-        mProps.billboardType = type;
+    setupConnections();
+}
+
+void ChildEditorWidget::BasicPropertiesWidget::setupConnections() {
+    // Is Follow
+    connect(&mFollowCheckBox, &QCheckBox::clicked, this, [this](bool checked) {
+        emit isFollowUpdated(checked);
+    });
+
+    // Is Parent Field
+    connect(&mParentFieldCheckBox, &QCheckBox::clicked, this, [this](bool checked) {
+        emit isParentFieldUpdated(checked);
+    });
+
+    // Billboard Type
+    connect(&mBillboardComboBox, &QComboBox::currentIndexChanged, this, [this]() {
+        const auto type = mBillboardComboBox.currentData().value<Ptcl::BillboardType>();
+
         // TODO:
-        // mProps.isPolygon = (type == Ptcl::BillboardType::PolygonXY || type == Ptcl::BillboardType::PolygonXZ);
         // mProps.isVelLook = (type == Ptcl::BillboardType::VelLook || type == Ptcl::BillboardType::VelLookPolygon);
         // mProps.isEmitterBillboardMtx = mProps.isVelLook;
+
+        mProps.billboardType = type;
+
+        const bool isPolygon = (type == Ptcl::BillboardType::PolygonXY || type == Ptcl::BillboardType::PolygonXZ);
+        emit isPolygonUpdated(isPolygon);
+
         emit propertiesUpdated(mProps);
     });
 }
 
-void ChildEditorWidget::BasicPropertiesWidget::setProperties(const Ptcl::ChildData::BasicProperties& properties) {
+void ChildEditorWidget::BasicPropertiesWidget::setProperties(const Ptcl::ChildData::BasicProperties& properties, bool isFollow, bool isParentField) {
     QSignalBlocker b1(mBillboardComboBox);
+    QSignalBlocker b2(mFollowCheckBox);
+    QSignalBlocker b3(mParentFieldCheckBox);
+
     mProps = properties;
-    mBillboardComboBox.setCurrentEnum(mProps.billboardType);
+
+    const s32 index = mBillboardComboBox.findData(QVariant::fromValue(mProps.billboardType));
+    mBillboardComboBox.setCurrentIndex(index);
+
+    mFollowCheckBox.setChecked(isFollow);
+    mParentFieldCheckBox.setChecked(isParentField);
+}
+
+void ChildEditorWidget::BasicPropertiesWidget::populateBillboardType() {
 }
 
 
