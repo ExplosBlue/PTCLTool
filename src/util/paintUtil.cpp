5e5f6276128f8f75df550c24444d9bb7dcb0f308
ExplosBlue <explosblue@gmail.com>
2026-02-08 09:19:55 +0000

    fix: use stable coords when drawing checkerboards

--- src/util/paintUtil.cpp
+++ src/util/paintUtil.cpp
@@ -21,11 +21,12 @@ void drawCheckerboard(QPainter& painter, const QRect& rect, s32 checkerSize, con
 
     for (s32 y = top; y < bottom; y += checkerSize) {
         for (s32 x = left; x < right; x += checkerSize) {
-            QRect cell(x, y, checkerSize, checkerSize);
+            const s32 localX = x - left;
+            const s32 localY = y - top;
 
-            bool isLight = ((x / checkerSize + y / checkerSize) & 1) == 0;
+            bool isLight = ((localX / checkerSize + localY / checkerSize) & 1) == 0;
 
-            painter.fillRect(cell, isLight ? light : dark);
+            painter.fillRect(QRect{x, y, checkerSize, checkerSize}, isLight ? light : dark);
         }
     }
 
