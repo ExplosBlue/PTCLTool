0514c4e7e667eb255f0e6a64706e0cc670a95800
ExplosBlue <explosblue@gmail.com>
2026-03-17 04:37:08 +0000

    refactor: change /emitterWidget to /inspector

---
+++ include/editor/inspector/scalePropertiesWidget.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "editor/components/animGraph.h"
+#include "editor/inspector/inspectorWidgetBase.h"
+
+#include <QWidget>
+
+
+namespace PtclEditor {
+
+
+// ========================================================================== //
+
+
+class ScalePropertiesWidget final : public InspectorWidgetBase {
+    Q_OBJECT
+public:
+    explicit ScalePropertiesWidget(QWidget* parent = nullptr);
+
+private:
+    void populateProperties() final;
+    void setupConnections();
+
+    void updateAnimPoint(s32 pointIndex, const AnimGraph::GraphPoint& point, f32 (Math::Vector2f::*get)() const);
+    void updateGraphs();
+
+private:
+    AnimGraph mGraphX{};
+    AnimGraph mGraphY{};
+
+    QDoubleSpinBox mRandSpinbox{};
+};
+
+
+// ========================================================================== //
+
+
+} // namespace PtclEditor
---
+++ src/editor/inspector/lifespanPropertiesWidget.cpp
@@ -0,0 +1,86 @@
+#include "editor/inspector/lifespanPropertiesWidget.h"
+
+#include <QFormLayout>
+
+
+namespace PtclEditor {
+
+
+// ========================================================================== //
+
+
+LifespanPropertiesWidget::LifespanPropertiesWidget(QWidget* parent) :
+    InspectorWidgetBase{parent} {
+
+    auto* mainLayout = new QFormLayout(this);
+
+    mLifeSpanSpinBox.setRange(0, sLifeInfinite - 1);
+    mLifeSpanSpinBox.setSuffix(" Frames");
+
+    // TODO: unsure that this is actually treated as a percentage
+    // Assumed based on behavior seen in EFT PTCL
+    mLifeSpanRndSpinBox.setRange(0, std::numeric_limits<s32>::max());
+    mLifeSpanRndSpinBox.setSuffix("%");
+
+    mainLayout->addRow("Infinite Life", &mInfiniteLifeCheckBox);
+
+    mainLayout->addRow("Lifespan:", &mLifeSpanSpinBox);
+    mainLayout->addRow("Lifespan Randomness:", &mLifeSpanRndSpinBox);
+
+    setupConnections();
+}
+
+void LifespanPropertiesWidget::setupConnections() {
+    connect(&mLifeSpanSpinBox, &QSpinBox::valueChanged, this, [this](u64 value) {
+        setEmitterProperty(
+            "Set LifeSpan",
+            "SetLifeSpan",
+            &Ptcl::Emitter::ptclLife,
+            &Ptcl::Emitter::setPtclLife,
+            static_cast<s32>(value)
+        );
+    });
+
+    connect(&mInfiniteLifeCheckBox, &QCheckBox::clicked, this, [this](bool checked) {
+        const s32 newLife = checked ? sLifeInfinite : 100;
+
+        setEmitterProperty(
+            "Toggle Infinite Life",
+            "SetParticleLife",
+            &Ptcl::Emitter::ptclLife,
+            &Ptcl::Emitter::setPtclLife,
+            newLife
+        );
+    });
+
+    connect(&mLifeSpanRndSpinBox, &QSpinBox::valueChanged, this, [this](u64 value) {
+        setEmitterProperty(
+            "Set LifeSpan Random",
+            "SetLifeSpanRand",
+            &Ptcl::Emitter::ptclLifeRandom,
+            &Ptcl::Emitter::setPtclLifeRandom,
+            static_cast<s32>(value)
+        );
+    });
+}
+
+void LifespanPropertiesWidget::populateProperties() {
+    QSignalBlocker b1(mInfiniteLifeCheckBox);
+    QSignalBlocker b2(mLifeSpanSpinBox);
+    QSignalBlocker b3(mLifeSpanRndSpinBox);
+
+    const s32 lifeSpan = mEmitter->ptclLife();
+    const bool infiniteLife = (lifeSpan == sLifeInfinite);
+
+    mLifeSpanSpinBox.setValue(lifeSpan);
+    mLifeSpanSpinBox.setDisabled(infiniteLife);
+    mInfiniteLifeCheckBox.setChecked(infiniteLife);
+
+    mLifeSpanRndSpinBox.setValue(mEmitter->ptclLifeRandom());
+}
+
+
+// ========================================================================== //
+
+
+} // namespace PtclEditor
