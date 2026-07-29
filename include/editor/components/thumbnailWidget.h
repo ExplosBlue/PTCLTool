af762dfc96a1455951d452d06f4d3c9079bdfbc9
ExplosBlue <explosblue@gmail.com>
2026-07-29 20:45:53 +0100

    feat(ui): make thumbnail widget dim unused space & scale checkerboard

--- include/editor/components/thumbnailWidget.h
+++ include/editor/components/thumbnailWidget.h
@@ -25,8 +25,16 @@ protected:
     void mousePressEvent(QMouseEvent* event) override;
 
 private:
+    void calcTextureRect();
+
+    void drawBackground(QPainter& painter) const;
+    void drawCheckerboard(QPainter& painter) const;
+    void drawTexture(QPainter& painter) const;
+
+private:
     QPixmap mPixmap{};
     QSize mThumbnailSize{64, 64};
+    QRect mTextureRect{};
 };
 
 
--- src/editor/components/thumbnailWidget.cpp
+++ src/editor/components/thumbnailWidget.cpp
@@ -16,17 +16,23 @@ ThumbnailWidget::ThumbnailWidget(QWidget* parent) :
 
 void ThumbnailWidget::setPixmap(const QPixmap& pixmap) {
     mPixmap = pixmap;
+
+    calcTextureRect();
     update();
 }
 
 void ThumbnailWidget::setThumbnailSize(QSize size) {
     mThumbnailSize = size;
     setFixedSize(size);
+
+    calcTextureRect();
     updateGeometry();
 }
 
 void ThumbnailWidget::clear() {
     mPixmap = {};
+
+    calcTextureRect();
     update();
 }
 
@@ -34,21 +40,66 @@ QSize ThumbnailWidget::sizeHint() const {
     return mThumbnailSize;
 }
 
-void ThumbnailWidget::paintEvent(QPaintEvent* /*event*/) {
+void ThumbnailWidget::calcTextureRect() {
+    if (mPixmap.isNull()) {
+        mTextureRect = {};
+    }
+
+    QSize scaledSize = mPixmap.size();
+    scaledSize.scale(mThumbnailSize, Qt::KeepAspectRatio);
+
+    mTextureRect = {
+        QPoint{
+            (width() - scaledSize.width()) / 2,
+            (height() - scaledSize.height()) / 2
+        },
+        scaledSize
+    };
+}
+
+void ThumbnailWidget::drawBackground(QPainter& painter) const {
+    painter.fillRect(rect(), palette().window().color());
+
+    QColor dim(0, 0, 0, 100);
+
+    QRegion outside(rect());
+    outside -= mTextureRect;
+
+    for (const QRect& r : outside) {
+        painter.fillRect(r, dim);
+    }
+}
+
+void ThumbnailWidget::drawCheckerboard(QPainter& painter) const {
+    f32 sx = static_cast<f32>(mTextureRect.width())  / static_cast<f32>(mPixmap.width());
+    f32 sy = static_cast<f32>(mTextureRect.height()) / static_cast<f32>(mPixmap.height());
+
+    f32 pixelsPerTexel = std::min(sx, sy);
+
+    s32 checkerSize = std::max(2, static_cast<s32>(std::round(pixelsPerTexel * 4.0f)));
+
+    PaintUtil::drawCheckerboard(painter, mTextureRect, checkerSize, mTextureRect.size());
+}
+
+
+void ThumbnailWidget::drawTexture(QPainter& painter) const {
+    if (mPixmap.isNull()) {
+        return;
+    }
+
+    painter.setRenderHint(QPainter::SmoothPixmapTransform, false);
+    painter.drawPixmap(mTextureRect, mPixmap);
+}
+
+void ThumbnailWidget::paintEvent(QPaintEvent* event) {
+    Q_UNUSED(event);
+
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
-    QRect bounds = rect();
-    PaintUtil::drawCheckerboard(painter, rect(), 8, mThumbnailSize);
-
-    if (!mPixmap.isNull()) {
-        QPixmap scaled = mPixmap.scaled(mThumbnailSize, Qt::KeepAspectRatio, Qt::FastTransformation);
-
-        int x = (bounds.width() - scaled.width()) / 2;
-        int y = (bounds.height() - scaled.height()) / 2;
-
-        painter.drawPixmap(x, y, scaled);
-    }
+    drawBackground(painter);
+    drawCheckerboard(painter);
+    drawTexture(painter);
 }
 
 void ThumbnailWidget::mousePressEvent(QMouseEvent* event) {
